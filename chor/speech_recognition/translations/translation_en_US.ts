84b46ff5fabb383b60196ea4068d26dadef91474
Jacob <jstrot21@gmail.com>
2022-01-12 08:53:09 -0500

    Choregraphe diagrams.

---
+++ chor/speech_recognition/translations/translation_en_US.ts
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>red</source>
+            <comment>Text</comment>
+            <translation type="obsolete">red</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>i am a nao robot ahahhaha</source>
+            <comment>Text</comment>
+            <translation type="unfinished">i am a nao robot ahahhaha</translation>
+        </message>
+    </context>
+</TS>
