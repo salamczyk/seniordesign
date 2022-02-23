034c02083baf9af378ba03094519d3fce6b0143f
Jacob <jstrot21@gmail.com>
2022-02-23 10:38:39 -0500

    Basic test using python.

---
+++ py-scripts/test/run.ps1
@@ -0,0 +1,12 @@
+
+# Execute multiple NAO robot python scripts
+
+$ip="127.0.0.1"
+$port1=9558
+$port2=9559
+
+pythonw C:\Users\Jacob\Documents\test.py --ip $ip --port $port1
+pythonw C:\Users\Jacob\Documents\test.py --ip $ip --port $port2
+
+ 
+
