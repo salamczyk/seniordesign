407e238b1c081806c8b4e35b1064d7d72507e134
Jacob <jstrot21@gmail.com>
2022-04-29 14:21:42 -0400

    Fixed file names.

--- py-scripts/espex/runNAOs.sh
+++ py-scripts/espex/runNAOs.sh
@@ -12,7 +12,7 @@ fileB="data/dataB$t.csv"
 mode='espex'
 
 # python2.7 test.py $ipA $portA $fileA $mode
-python2.7 statemachineA-JS.py $ipA $portA $fileA $mode &
+python2.7 statemachineA.py $ipA $portA $fileA $mode &
 sleep 3
-python2.7 statemachineB-JS.py $ipB $portB $fileB $mode
+python2.7 statemachineB.py $ipB $portB $fileB $mode
 python3 processData.py $fileA $fileB $t
